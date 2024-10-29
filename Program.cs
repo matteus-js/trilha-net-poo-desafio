4e91452e7fbcdc35f58b648961affb8f2b84befd
Matteus Santos <matteus.py@gmail.com>
2024-10-29 19:05:43 -0300

    resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("123456", "modelo 1", "11111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("DevTools");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone("981423", "XP", "222222222", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
